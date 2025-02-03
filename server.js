c7b0bb670084939890750b68873cb142ba46b90a
uhiaha888 <uhiaha888@gmail.com>
2025-02-03 17:18:50 +0900

    メール送信

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const express = require('express');
+
+import { sendEmail } from './mail.js';
+import express from 'express';
 const app = express();
 
 // Render では環境変数 PORT が設定されるので、設定されていなければ 3000 番ポートを利用
@@ -6,6 +8,7 @@ const port = process.env.PORT || 3000;
 
 // ルートへのリクエストに対して "Hello, world!" を返す
 app.get('/', (req, res) => {
+  sendEmail();
   res.send('Hello, world!');
 });
 
